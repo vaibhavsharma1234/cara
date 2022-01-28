a9c8379ea6899bb5e7d011e6cdf598deb4893e6d
vaibhavsharma1234 <96341386+vaibhavsharma1234@users.noreply.github.com>
2022-01-29 02:39:17 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,43 @@
+const bar = document.getElementById('bar');
+const close = document.getElementById('close');
+const nav = document.getElementById('navbar');
+
+if (bar) {
+    bar.addEventListener("click", function() {
+        nav.classList.add('active');
+    }); 
+}
+
+if (close) {
+    close.addEventListener("click", function() {
+        nav.classList.remove('active');
+    }); 
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
